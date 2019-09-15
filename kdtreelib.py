b40be5db4d259ec4649a0ea7f581dd304307c176
crscillitoe <crscillitoe@wpi.edu>
2019-09-15 19:26:34 -0400

    begin modifying find_nearest_neighbor

--- kdtreelib.py
+++ kdtreelib.py
@@ -8,7 +8,7 @@ class KDValueMapping:
         self.value = value
 
 class KDNode:
-    def __init__(self, value: KDValueMapping, left: KDNode, right: KDNode):
+    def __init__(self, value: KDValueMapping, left, right):
         self.value_mapping = value
         self.right = right
         self.left = left
@@ -42,27 +42,22 @@ class KDTree:
         return sqrt(reduce(lambda x,y: x + y, distances))
 
     def find_nearest_neighbor(self, search_node: Tuple):
-        return self._find_nearest_neighbor(search_node, self.root)
+        return self._find_nearest_neighbor(search_node, self.root, 0)
 
-    def _find_nearest_neighbor(self, search_node: Tuple, root: KDNode) -> any:
+    def _find_nearest_neighbor(self, search_node: Tuple, root: KDNode, depth: int) -> any:
         """
         Finds the nearest neighbor to the given node in the balanced tree
         """
-        min_distance: int = self.distance(root.value_mapping.point, search_node)
-        smallest_child = root
-
-        left_distance = self.distance(root.left.value_mapping.point, search_node)
-        right_distance = self.distance(root.right.value_mapping.point, search_node)
-        for i in [root.left, root.right]:
-            if i is None:
-                continue
-
-            child_distance: int = self.distance(i.value_mapping.point, search_node)
-            if child_distance < min_distance:
-                smallest_child = i
-                min_distance = child_distance
-
-        if smallest_child == root:
+        if root.left is None and root.right is None:
             return root.value_mapping
 
-        return self.find_nearest_value(search_node, smallest_child)
+        axis = depth % self.num_dimensions
+        if root.left is not None:
+            if root.left.value_mapping.point[axis] < search_node[axis]:
+                return self._find_nearest_neighbor(search_node, root.left, depth + 1)
+
+        if root.right is not None:
+            if root.right.value_mapping.point[axis] <= search_node[axis]:
+                return self._find_nearest_neighbor(search_node, root.right, depth + 1)
+
+        return root.value_mapping
