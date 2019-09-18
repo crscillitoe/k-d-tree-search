0cd36471f53a151524886230b15b4e804463176e
Bradford Bonanno <bwbonanno@wpi.edu>
2019-09-17 21:06:35 -0400

    Working prototype

--- kdtreelib.py
+++ kdtreelib.py
@@ -2,10 +2,17 @@ from typing import *
 from math import sqrt
 from functools import reduce
 
+class IncorrectDimensionsError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 class KDValueMapping:
     def __init__(self, point: Tuple[int, ...], value: any):
         self.point = point
         self.value = value
+    
+    def __repr__(self):
+        return f"<{self.point}, {self.value}>"
 
 class KDNode:
     def __init__(self, value: KDValueMapping, left: 'KDNode', right: 'KDNode'):
@@ -13,9 +20,13 @@ class KDNode:
         self.right = right
         self.left = left
 
+    def __repr__(self):
+        return self.value_mapping.__repr__()
+
 class KDTree:
     def __init__(self, num_dimensions: int, point_list: List[KDValueMapping]):
         self.num_dimensions = num_dimensions
+        self.tree_size = 0
         self.root = self._build_tree(point_list)
 
     def _build_tree(self, point_list: List[KDValueMapping], depth: int=0) -> KDNode:
@@ -26,6 +37,7 @@ class KDTree:
         point_list.sort(key=lambda x: x.point[axis])
 
         median = len(point_list) // 2
+        self.tree_size += 1
         return KDNode(
             value=point_list[median],
             left=self._build_tree(point_list[:median], depth + 1),
@@ -42,6 +54,8 @@ class KDTree:
         return sqrt(reduce(lambda x,y: x + y, distances))
 
     def find_nearest_neighbor(self, search_node: Tuple[int, ...]) -> KDValueMapping:
+        if len(search_node) != self.num_dimensions:
+            raise IncorrectDimensionsError(f"Expected {self.num_dimensions} dimensions, received {len(search_node)}")
         return self._find_nearest_neighbor(search_node, self.root, 0).value_mapping
 
     def _find_nearest_neighbor(self, search_node: Tuple[int, ...], root: KDNode, depth: int) -> KDNode:
@@ -51,15 +65,17 @@ class KDTree:
         if root.left is None and root.right is None:
             return root
 
+        if root.value_mapping.point == search_node:
+            return root
+
         bottom_node = root
         axis = depth % self.num_dimensions
-        if root.left is not None:
-            if root.left.value_mapping.point[axis] < search_node[axis]:
-                bottom_node = self._find_nearest_neighbor(search_node, root.left, depth + 1)
+        root_point = root.value_mapping.point
 
-        if root.right is not None:
-            if root.right.value_mapping.point[axis] > search_node[axis]:
-                bottom_node = self._find_nearest_neighbor(search_node, root.right, depth + 1)
+        if root.left is not None and  search_node[axis]<= root_point[axis]:
+            bottom_node = self._find_nearest_neighbor(search_node, root.left, depth + 1)
+        elif root.right is not None and search_node[axis] > root_point[axis]:
+            bottom_node = self._find_nearest_neighbor(search_node, root.right, depth + 1)
 
         return (bottom_node if self.distance(bottom_node.value_mapping.point, search_node) <
                                self.distance(root.value_mapping.point, search_node)
--- test_kdtreelib.py
+++ test_kdtreelib.py
@@ -1,15 +1,55 @@
 import unittest
 from testing_dictionary import color_dictionary
-from kdtreelib import KDNode, KDTree, KDValueMapping
+from kdtreelib import KDNode, KDTree, KDValueMapping, IncorrectDimensionsError
+from typing import *
 
 class TestKDTree(unittest.TestCase):
-    def test_red_emoji_finder(self):
+    @classmethod
+    def setUpClass(cls):
+        cls.five_dimensional_dict: Dict[Tuple[int,...], str] = {
+            (0,0,0,0,0): "N1",
+            (1,1,1,1,1): "N2",
+            (3,1,-2,4,5): "N3",
+            (100,100,100,100,100): "N4",
+            (-100,-100,-100,50,50): "N5",
+            (-100,-100,-100,-50,-50): "N6"
+        }
+        return super().setUpClass()
+
+    def test_exact_emoji(self):
         value_mappings: List[KDValueMapping] = []
         for i in color_dictionary.keys():
             value_mappings.append(KDValueMapping(point=i, value=color_dictionary[i]))
 
         tree = KDTree(num_dimensions=3, point_list=value_mappings)
+        self.assertEqual(tree.tree_size, len(value_mappings))
         self.assertEqual(tree.find_nearest_neighbor((133, 118, 74)).value, ':older_woman:')
+
+    def test_almost_exact_emoji(self):
+        value_mappings: List[KDValueMapping] = []
+        for i in color_dictionary.keys():
+            value_mappings.append(KDValueMapping(point=i, value=color_dictionary[i]))
+
+        tree = KDTree(num_dimensions=3, point_list=value_mappings)
+        self.assertEqual(tree.tree_size, len(value_mappings))
+        self.assertEqual(tree.find_nearest_neighbor((133, 118, 75)).value, ':older_woman:')
+
+    def test_incorrect_number_of_dimensions(self):
+        value_mappings: List[KDValueMapping] = []
+        for i in self.five_dimensional_dict.keys():
+            value_mappings.append(KDValueMapping(point=i, value=self.five_dimensional_dict[i]))
+        tree = KDTree(num_dimensions=5, point_list=value_mappings)
+        self.assertEqual(tree.tree_size, len(value_mappings))
+        with self.assertRaises(IncorrectDimensionsError):
+            tree.find_nearest_neighbor((50,50,50,50))
+
+    def test_five_dimensional_tree(self):
+        value_mappings: List[KDValueMapping] = []
+        for i in self.five_dimensional_dict.keys():
+            value_mappings.append(KDValueMapping(point=i, value=self.five_dimensional_dict[i]))
+        tree = KDTree(num_dimensions=5, point_list=value_mappings)
+        self.assertEqual(tree.tree_size, len(value_mappings))
+        self.assertEqual(tree.find_nearest_neighbor((50, 50, 50, 50, 50)).value, "N3")
 
 if __name__ == "__main__":
     unittest.main()
